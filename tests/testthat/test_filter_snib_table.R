f568b9abbc19633d447745875a595f8848960130
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-05 21:28:04 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_filter_snib_table.R
+++ tests/testthat/test_filter_snib_table.R
@@ -1,9 +1,15 @@
 describe("Filter SNIB table", {
+  snib_table <- readr::read_csv("/workdir/tests/data/snib_table_for_tests.csv", show_col_type = FALSE)
   it("Filter table by phylum", {
-    snib_table <- readr::read_csv("/workdir/tests/data/snib_table_for_tests.csv", show_col_type = FALSE)
     phylum <- "Tracheophyta"
     obtained <- filter_by_phylum(snib_table, phylum)
     expected_n_rows <- 4
     expect_equal(nrow(obtained), expected_n_rows)
+  })
+  it("Select columns of interes", {
+    obtained <- select_species_name_and_family(snib_table)
+    expected_columns <- c("familiavalida", "especievalida")
+    obtained_columns <- colnames(obtained)
+    expect_equal(obtained_columns, expected_columns)
   })
 })
