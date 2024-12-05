b36c4de5857b1da0240a9bfa17dfa73b35ca00ab
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-05 21:16:57 +0000

    🛑🧪 Fail tests

---
+++ tests/testthat/test_filter_snib_table.R
@@ -0,0 +1,9 @@
+describe("Filter SNIB table", {
+  it("Filter table by phylum", {
+    snib_table <- read_csv("/workdir/geci_diversity/tests/data/snib_table_for_tests.csv", show_col_type = FALSE)
+    phylum <- "Tracheophyta"
+    obtained <- filter_by_phylum(snib_table, phylum)
+    expected_n_rows <- 4
+    expect_equal(nrow(obtained), expected_n_rows)
+  })
+})
