5404dfae608c96355e8529daccf93602ce847cbe
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-05 23:00:16 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -1,9 +1,11 @@
 describe("Cli for module", {
   it("Cli for Jaccard Index", {
-    a_community_counts_path <- "/workdir/tests/data/community_a_for tests.csv"
-    b_community_counts_path <- "/workdir/tests/data/community_b_for tests.csv"
+    a_community_counts_path <- "/workdir/tests/data/community_a_for_tests.csv"
+    b_community_counts_path <- "/workdir/tests/data/community_b_for_tests.csv"
     output_path <- "/workdir/tests/data/jaccard_diversity.json"
     options <- list("newest_count_path" = a_community_counts_path, "previous_count_path" = b_community_counts_path, "results_path" = output_path)
+    testtools::if_exist_remove(output_path)
     write_jaccard_index_result(options)
+    expect_true(testtools::exist_output_file(output_path))
   })
 })
