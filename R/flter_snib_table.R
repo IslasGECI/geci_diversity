d8d77309d319c2401e169904597bef21989026c6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-05 21:44:26 +0000

    ✅ Pass tests

--- R/flter_snib_table.R
+++ R/flter_snib_table.R
@@ -1,8 +1,9 @@
+#' @export
 get_observed_species_by_phylum <- function(snib_table, phylum) {
   snib_table |>
     filter_by_phylum(phylum) |>
     select_species_name_and_family() |>
-    dplyr::distinct(especievalida)
+    dplyr::distinct(especievalida, .keep_all = TRUE)
 }
 
 filter_by_phylum <- function(snib_data, phylum) {
