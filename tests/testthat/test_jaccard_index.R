99f6afdc1558b2787e8b4837432e2d676c79ea81
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-05 22:32:03 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_jaccard_index.R
+++ tests/testthat/test_jaccard_index.R
@@ -1,7 +1,12 @@
 describe("Calculate Jaccard index", {
+  a_community <- tibble::tibble(Especie = c("sp_1", "sp_2", "sp_3", "sp_4"))
+  b_community <- tibble::tibble(Especie = c("sp_1", "sp_3", "sp_4", "sp_5"))
+  it("calculate jaccard index", {
+    obtained_index <- calculate_jaccard_index(a_community, b_community)
+    expected_index <- (3 / 5) * 100
+    expect_equal(obtained_index, expected_index)
+  })
   it("Get common species (C parameter)", {
-    a_community <- tibble::tibble(Especie = c("sp_1", "sp_2", "sp_3", "sp_4"))
-    b_community <- tibble::tibble(Especie = c("sp_1", "sp_3", "sp_4", "sp_5"))
     obatined <- get_number_of_common_species(a_community, b_community)
     expected <- 3
     expect_equal(obatined, expected)
