4bcbd1b0ad1ec0a635c6de9f1dd38e4e33224c5d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-05 23:05:57 +0000

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -2,6 +2,12 @@ write_jaccard_index_result <- function(options) {
   a_community <- readr::read_csv(options[["previous_count_path"]], show_col_types = FALSE)
   b_community <- readr::read_csv(options[["newest_count_path"]], show_col_types = FALSE)
   jaccard_index <- calculate_jaccard_index(a_community, b_community)
+  A <- nrow(a_community)
+  B <- nrow(b_community)
   C <- get_number_of_common_species(a_community, b_community)
-  list("A" = nrow(a_community), "B" = nrow(b_community), "C" = C, "jaccard_index" = jaccard_index)
+
+  output_content <- list("A" = A, "B" = B, "C" = C, "jaccard_index" = jaccard_index)
+  output_content |>
+    rjson::toJSON() |>
+    write(options[["results_path"]])
 }
