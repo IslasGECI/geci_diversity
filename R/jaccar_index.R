43cc8ae02336fd7d2189cc2422a4d051a20b9d52
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-05 22:33:50 +0000

    ✅ Pass tests

--- R/jaccar_index.R
+++ R/jaccar_index.R
@@ -1,3 +1,10 @@
+calculate_jaccard_index <- function(a_community, b_community) {
+  A <- nrow(a_community)
+  B <- nrow(b_community)
+  C <- get_number_of_common_species(a_community, b_community)
+  (C / (A + B - C)) * 100
+}
+
 get_number_of_common_species <- function(a_community, b_community) {
   dplyr::inner_join(a_community, b_community) |> nrow()
 }
