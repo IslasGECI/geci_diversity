72a91bdc9918c8e98715dedd872932c90fc25f86
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-06 21:31:25 +0000

    ♻️  Refactor

--- R/jaccar_index.R
+++ R/jaccar_index.R
@@ -6,5 +6,9 @@ calculate_jaccard_index <- function(a_community, b_community) {
 }
 
 get_number_of_common_species <- function(a_community, b_community) {
-  dplyr::inner_join(a_community, b_community) |> nrow()
+  list_unique_species_on_both(a_community, b_community) |> nrow()
+}
+
+list_unique_species_on_both <- function(a_community, b_community) {
+  dplyr::inner_join(a_community, b_community)
 }
