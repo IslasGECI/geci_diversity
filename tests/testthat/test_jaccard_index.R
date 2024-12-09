3296163482e95c94d211e03add2c76ee71ce80df
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-09 18:22:06 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_jaccard_index.R
+++ tests/testthat/test_jaccard_index.R
@@ -1,6 +1,6 @@
+a_community <- tibble::tibble(Especie = c("sp_1", "sp_2", "sp_3", "sp_4"))
+b_community <- tibble::tibble(Especie = c("sp_1", "sp_3", "sp_4", "sp_5"))
 describe("Calculate Jaccard index", {
-  a_community <- tibble::tibble(Especie = c("sp_1", "sp_2", "sp_3", "sp_4"))
-  b_community <- tibble::tibble(Especie = c("sp_1", "sp_3", "sp_4", "sp_5"))
   it("calculate jaccard index", {
     obtained_index <- calculate_jaccard_index(a_community, b_community)
     expected_index <- (3 / 5) * 100
@@ -10,5 +10,13 @@ describe("Calculate Jaccard index", {
     obatined <- get_number_of_common_species(a_community, b_community)
     expected <- 3
     expect_equal(obatined, expected)
+  })
+})
+
+describe("Get full list of species from two lists", {
+  it("Get all unique species", {
+    obtained <- list_unique_species_on_both(a_community, b_community)
+    expected_nrow <- 5
+    expect_equal(nrow(obtained), expected_nrow)
   })
 })
