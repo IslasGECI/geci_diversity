0a095552ea745c4f5ca8c86e7eeeaea9bd02e2a9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-09 18:34:06 +0000

    ♻️  Refactor

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -1,7 +1,7 @@
 describe("Cli for module", {
+  a_community_counts_path <- "/workdir/tests/data/community_a_for_tests.csv"
+  b_community_counts_path <- "/workdir/tests/data/community_b_for_tests.csv"
   it("Cli for Jaccard Index", {
-    a_community_counts_path <- "/workdir/tests/data/community_a_for_tests.csv"
-    b_community_counts_path <- "/workdir/tests/data/community_b_for_tests.csv"
     output_path <- "/workdir/tests/data/jaccard_diversity.json"
     options <- list("newest_count_path" = a_community_counts_path, "previous_count_path" = b_community_counts_path, "results_path" = output_path)
     testtools::if_exist_remove(output_path)
@@ -9,8 +9,6 @@ describe("Cli for module", {
     expect_true(testtools::exist_output_file(output_path))
   })
   it("Cli for write_list_of_species_on_both", {
-    a_community_counts_path <- "/workdir/tests/data/community_a_for_tests.csv"
-    b_community_counts_path <- "/workdir/tests/data/community_b_for_tests.csv"
     output_path <- "/workdir/tests/data/unique_species_on_both_lists.csv"
     options <- list("count-path-one" = a_community_counts_path, "count-path-two" = b_community_counts_path, "results-path" = output_path)
     testtools::if_exist_remove(output_path)
