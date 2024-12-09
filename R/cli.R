7619ef721b22bb4dfdda14d1d7fa17ce6dc3378f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-09 18:47:41 +0000

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -18,3 +18,9 @@ write_list_of_species_on_both <- function(options) {
   list_unique_species_on_both(a_community, b_community) |>
     readr::write_csv(options[["results-path"]])
 }
+
+get_domain_specific_options <- function() {
+  results_path <- geci.optparse::character_option(c("-r", "--results-path"), default = "/workdir/reports/tables/result.csv", help = "File path of the desire output")
+  option_names <- c(results_path)
+  geci.optparse::get_options_from_vec(option_names)
+}
