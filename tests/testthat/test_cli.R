c3ca828b745d7240ab1d43f5e5aa5bb62011dbae
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-09 18:13:11 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -8,4 +8,13 @@ describe("Cli for module", {
     write_jaccard_index_result(options)
     expect_true(testtools::exist_output_file(output_path))
   })
+  it("Cli for write_list_of_species_on_both", {
+    a_community_counts_path <- "/workdir/tests/data/community_a_for_tests.csv"
+    b_community_counts_path <- "/workdir/tests/data/community_b_for_tests.csv"
+    output_path <- "/workdir/tests/data/unique_species_on_both_lists.csv"
+    options <- list("count-path-one" = a_community_counts_path, "count-path-two" = b_community_counts_path, "results-path" = output_path)
+    testtools::if_exist_remove(output_path)
+    write_list_of_species_on_both(options)
+    expect_true(testtools::exist_output_file(output_path))
+  })
 })
